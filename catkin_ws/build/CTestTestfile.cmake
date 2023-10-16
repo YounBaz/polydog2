8ae45a3308a875d71ccad3eee7b46ad7a6211e76
YounesBazi <younes-bazi@outlook.fr>
2023-10-16 23:59:54 +0200

    .

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/younes/catkin_ws/src
-# Build directory: /home/younes/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("polydog")
